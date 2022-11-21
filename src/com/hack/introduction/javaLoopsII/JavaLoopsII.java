45f5f953c84c3b0ac4f78aa833fc13b8758ed14b
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2022-11-21 19:55:40 +0600

    Java Loops II

---
+++ src/com/hack/introduction/javaLoopsII/JavaLoopsII.java
@@ -0,0 +1,21 @@
+package com.hack.introduction.javaLoopsII;
+
+import java.util.Scanner;
+
+public class JavaLoopsII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            for (int j = 0; j < n; j++) {
+                a += (int) (Math.pow(2, j) * b);
+                System.out.print(Integer.toString(a) + " ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
