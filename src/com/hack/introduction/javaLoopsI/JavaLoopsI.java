9fcb8ddb94454363b86cd20f22bed06814098d57
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2022-11-21 14:05:43 +0600

    Java Loops I

---
+++ src/com/hack/introduction/javaLoopsI/JavaLoopsI.java
@@ -0,0 +1,17 @@
+package com.hack.introduction.javaLoopsI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoopsI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+
+        bufferedReader.close();
+    }
+}
