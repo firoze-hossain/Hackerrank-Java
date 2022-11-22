2512c4b23a3bf2f4ffa66b1fe2cd7dc5f9548ae8
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2022-11-22 10:09:06 +0600

    Java Datatypes

---
+++ src/com/hack/introduction/javaDatatypes/JavaDatatypes.java
@@ -0,0 +1,30 @@
+package com.hack.introduction.javaDatatypes;
+
+import java.util.Scanner;
+
+public class JavaDatatypes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            try {
+                long n = sc.nextLong();
+                System.out.println(n + " can be fitted in:");
+                if (n >= -128 && n <= 127) {
+                    System.out.println("* byte");
+                }
+                if (n >= -32768 && n <= 32767) {
+                    System.out.println("* short");
+                }
+                if (n >= -(Math.pow(2, 31)) && n <= (Math.pow(2, 31) - 1)) {
+                    System.out.println("* int");
+                }
+                if (n >= -(Math.pow(2, 63)) && n <= (Math.pow(2, 63) - 1)) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
