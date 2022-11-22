d38ee8ab504a79b7bff20f6ad7e1892b23d9b06d
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2022-11-22 12:36:33 +0600

    Java Static Initializer Block

--- src/com/hack/introduction/javaStaticInitializerBlock/JavaStaticInitializerBlock.java
+++ src/com/hack/introduction/javaStaticInitializerBlock/JavaStaticInitializerBlock.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class JavaStaticInitializerBlock {
     public static void main(String[] args) {
+        int b, h;
         Scanner sc = new Scanner(System.in);
-        int b = sc.nextInt();
-        int h = sc.nextInt();
-        if (b < 0 || h < 0) {
+        b = sc.nextInt();
+        sc.nextLine();
+        h = sc.nextInt();
+        areaParallelogram(b, h);
+        sc.close();
+    }
+
+    public static void areaParallelogram(int b, int h) {
+        if (b <= 0 || h <= 0) {
             System.out.println("java.lang.Exception: Breadth and height must be positive");
         } else {
             System.out.println(b * h);
         }
+
     }
 }
