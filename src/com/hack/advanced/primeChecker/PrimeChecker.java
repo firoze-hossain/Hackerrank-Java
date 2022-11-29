4a384342ab88e721f36a41c6209ccaaf5c04706f
Md. Firoze Hossain <firoze.hossain01@yahoo.com>
2022-11-29 21:37:44 +0600

    Prime Checker

--- src/com/hack/advanced/primeChecker/PrimeChecker.java
+++ src/com/hack/advanced/primeChecker/PrimeChecker.java
@@ -1,7 +1,36 @@
 package com.hack.advanced.primeChecker;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
 public class PrimeChecker {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        int num3 = Integer.parseInt(br.readLine());
+        int num4 = Integer.parseInt(br.readLine());
+        int num5 = Integer.parseInt(br.readLine());
+        Prime p = new Prime();
+        p.checkPrime(num1);
+        p.checkPrime(num1, num2);
+        p.checkPrime(num1, num2, num3);
+        p.checkPrime(num1, num2, num3, num4, num5);
+
+    }
+}
+
+class Prime {
+    public void checkPrime(int... num) {
+        for (int n : num) {
+            BigInteger bg = BigInteger.valueOf(n);
+            if (bg.isProbablePrime(1)) {
+                System.out.printf("%s ", bg);
+            }
+        }
+        System.out.println();
+
 
     }
 }
